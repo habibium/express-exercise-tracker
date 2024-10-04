44c30f4088731201b5c3b17dd4ecf966871f408c
habibium <habib.prodev@gmail.com>
2024-10-05 03:18:12 +0600

    chore: install and setup body parser

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+app.route("/api/users").post((req, res) => {
+  const username = req.body?.username;
+  console.log(username);
+  res.send(username);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `Your app is listening on http://localhost:${process.env.port ?? 3000}`
+  );
 });
