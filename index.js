9e920420c12cab939d12ee127d27668bc9e6ad0d
habibium <habib.prodev@gmail.com>
2024-10-05 03:35:47 +0600

    feat: implement user creation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
+const { createUser } = require("./db");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,10 +12,13 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.route("/api/users").post((req, res) => {
+app.route("/api/users").post(async (req, res) => {
   const username = req.body?.username;
-  console.log(username);
-  res.send(username);
+  const newUser = await createUser(username);
+  res.json({
+    username: newUser?.username,
+    _id: newUser?._id,
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
